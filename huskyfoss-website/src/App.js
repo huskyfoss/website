956232bb3b68b4b5af781929ffad60346377b792
rgoyal17 <rgoyal17@uw.edu>
2020-09-20 16:12:31 +0530

    setup

---
+++ huskyfoss-website/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Hello                
+            </div>
+        );
+    }
+
+}
+export default App;
